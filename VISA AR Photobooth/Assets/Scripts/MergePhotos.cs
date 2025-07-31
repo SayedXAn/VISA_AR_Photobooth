0fb5358c08bc838864008659ef8840c0241c092f
SayedXAn <106014255+SayedXAn@users.noreply.github.com>
2025-07-31 13:36:44 +0600

    Hang fixed, full functioning

--- VISA AR Photobooth/Assets/Scripts/MergePhotos.cs
+++ VISA AR Photobooth/Assets/Scripts/MergePhotos.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 using UnityEngine.Networking;
 using Newtonsoft.Json;
 using System.IO;
+using System.Threading;
+using System.Linq;
 
 public class MergePhotos : MonoBehaviour
 {
@@ -20,6 +22,7 @@ public class MergePhotos : MonoBehaviour
     public GameObject finalScreenshotCanvas;
     public GameObject finalPanel;
     int selectedFrameIndex;
+    private int pendingEncodes = 0;
 
     //for screenshots
     private int resWidth = 1080; //500
@@ -34,12 +37,6 @@ public class MergePhotos : MonoBehaviour
     List<byte[]> allImagesBytes = new List<byte[]>();
 
 
-    //private int ssResWidth = 1080;
-    //private int ssResHeight = 1920;
-    //private Texture2D frameScreenshots;
-    //public Camera framesScrCamera;
-
-
     void Start()
     {
         //totalSlots = imageSlots.Length;
@@ -47,189 +44,29 @@ public class MergePhotos : MonoBehaviour
         //DisplayImages();
     }
 
-    //public void DisplayImages()
-    //{
-    //    for(int i=0; i < totalSlots; i++)
-    //    {
-    //        imageSlots[i].transform.GetChild(0).GetComponent<Image>().sprite = backgroundImages[i];
-    //        imageSlots[i].transform.GetChild(1).gameObject.SetActive(true);/*GetComponent<Image>().sprite = transparentImage;*/
-    //        imageSlots[i].transform.GetChild(1).GetComponent<Image>().material = transMat;
-    //        imageSlots[i].transform.GetChild(2).GetComponent<Image>().sprite = foregroundImages[i];
-    //    }
-    //}
-
-    public void ProceedButtonPressed()
-    {
-        StartCoroutine(TakeScreenShot());
-    }
-
     public void OnFrameSelected(int index)
     {
         UIManager.instance.defaultBG.SetActive(true);
         selectedFrameIndex = index;
-        UIManager.instance.chooseFramesPanel.SetActive(false);
+        StartCoroutine(TakeScreenShot());
     }
-
-    //IEnumerator TakeScreenShot()
-    //{
-    //    GameObject selectedFrame = Instantiate(imageSlots[selectedFrameIndex], finalScreenshotCanvas.transform);
-
-    //    RectTransform rectTransform = selectedFrame.GetComponent<RectTransform>();
-    //    rectTransform.anchorMin = new Vector2(0, 0);
-    //    rectTransform.anchorMax = new Vector2(1, 1);
-    //    rectTransform.offsetMin = new Vector2(0, 0);
-    //    rectTransform.offsetMax = new Vector2(0, 0);
-
-
-    //    finalScreenshotCanvas.SetActive(true);
-    //    finalScreenShotCamera.SetActive(true);
-
-    //    //UIManager.instance.chooseFramesPanel.SetActive(false);
-
-    //    yield return new WaitForEndOfFrame();
-
-    //    RenderTexture rt = new RenderTexture(resWidth, resHeight, 24);
-    //    scrCamera.targetTexture = rt;
-    //    screenShot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
-    //    scrCamera.Render();
-    //    RenderTexture.active = rt;
-    //    screenShot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
-    //    screenShot.Apply();
-
-    //    scrCamera.targetTexture = null;
-    //    RenderTexture.active = null; // JC: added to avoid errors
-    //    Destroy(rt);
-    //    bytes = screenShot.EncodeToPNG();
-
-    //    string directoryPath = (Application.dataPath + "/../Chosen Photos");
-    //    if (!Directory.Exists(directoryPath))
-    //    {
-    //        //if it doesn't, create it
-    //        Directory.CreateDirectory(directoryPath);
-
-    //    }
-
-    //    Destroy(selectedFrame);
-
-    //    string allImagesDir = (Application.dataPath + "/../All_Photos");
-    //    if (!Directory.Exists(allImagesDir))
-    //    {
-    //        //if it doesn't, create it
-    //        Directory.CreateDirectory(allImagesDir);
-
-    //    }
-
-    //    allImagesBytes.Clear();
-
-    //    foreach (GameObject img in imageSlots)
-    //    {
-    //        GameObject currFrame = Instantiate(img, finalScreenshotCanvas.transform);
-
-    //        RectTransform _rectTransform = currFrame.GetComponent<RectTransform>();
-    //        _rectTransform.anchorMin = new Vector2(0, 0);
-    //        _rectTransform.anchorMax = new Vector2(1, 1);
-    //        _rectTransform.offsetMin = new Vector2(0, 0);
-    //        _rectTransform.offsetMax = new Vector2(0, 0);
-
-
-    //        finalScreenshotCanvas.SetActive(true);
-    //        finalScreenShotCamera.SetActive(true);
-
-    //        //UIManager.instance.chooseFramesPanel.SetActive(false);
-
-    //        yield return new WaitForEndOfFrame();
-
-    //        RenderTexture _rt = new RenderTexture(resWidth, resHeight, 24);
-    //        scrCamera.targetTexture = _rt;
-    //        screenShot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
-    //        scrCamera.Render();
-    //        RenderTexture.active = _rt;
-    //        screenShot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
-    //        screenShot.Apply();
-
-    //        scrCamera.targetTexture = null;
-    //        RenderTexture.active = null; // JC: added to avoid errors
-    //        Destroy(_rt);
-    //        byte[] b = screenShot.EncodeToPNG();
-    //        allImagesBytes.Add(b);
-    //    }
-
-    //    WWWForm form = new WWWForm();
-    //    //save locally
-    //    string imageName = "IMG_With_Chosen_Frame-" + System.DateTime.Now.ToString("HH-mm-ss-dd-MM-yyyy") + "choosen.png";
-    //    System.IO.File.WriteAllBytes(directoryPath + "/" + imageName, bytes);
-    //    for (int i = 0; i < allImagesBytes.Count; i++)
-    //    {
-    //        string _imageName = "IMG_all_four-" + System.DateTime.Now.ToString("HH-mm-ss-dd-MM-yyyy") + $"_{i}.png";
-    //        File.WriteAllBytes(allImagesDir + "/" + _imageName, allImagesBytes[i]);
-    //        form.AddBinaryData("images", allImagesBytes[i], imageName, "image/png");
-    //    }
-    //    form.AddField("title", "VISA");
-    //    form.AddField("description", "Test description");
-
-    //    //form.AddBinaryData("image", bytes, imageName, "image/png");
-    //    Debug.Log("Sending............");
-
-    //    UnityWebRequest request = UnityWebRequest.Post($"https://sing-file-share.wskoly.xyz/api/upload/", form);
-    //    request.SetRequestHeader("API-Key", API_KEY);
-    //    yield return request.SendWebRequest();
-
-    //    finalScreenshotCanvas.SetActive(false);
-    //    finalScreenShotCamera.SetActive(false);
-
-    //    if (!request.isNetworkError && !request.isHttpError)
-    //    {
-    //        string responseText = request.downloadHandler.text;
-    //        Debug.Log(responseText);
-    //        SingFileApiResponse response = null;
-    //        try
-    //        {
-    //            response = JsonConvert.DeserializeObject<SingFileApiResponse>(responseText);
-    //        }
-    //        catch (System.Exception e)
-    //        {
-    //            Debug.Log(e.Message);
-    //        }
-
-    //        if (response != null)
-    //        {
-    //            string url = response.link;
-    //            //imageUrlText.text = url;
-    //            QR_DisplayQuad.GetComponent<EasyQRCode>().textToEncode = url;
-    //            //call to generate QRCode here
-    //            QR_DisplayQuad.SetActive(true);
-    //            finalPanel.SetActive(true);
-    //            //Texture2D texture2D = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
-    //            //texture2D.LoadImage(request.downloadHandler.data);
-    //            //transMat.mainTexture = texture2D;
-    //            //GetURL(request.downloadHandler.text);
-    //        }
-    //    }
-    //    else
-    //    {
-    //        Debug.Log(request.error);
-    //        Debug.Log(request.downloadHandler.text);
-    //        NetworkError();
-    //    }
-    //    //make the printer workable
-
-    //    UIManager.instance.finalPanel.SetActive(true);
-    //}
-
 
     IEnumerator TakeScreenShot()
     {
-        // Step 1: Capture chosen frame
+
         GameObject selectedFrame = Instantiate(imageSlots[selectedFrameIndex], finalScreenshotCanvas.transform);
 
         RectTransform rectTransform = selectedFrame.GetComponent<RectTransform>();
-        rectTransform.anchorMin = Vector2.zero;
-        rectTransform.anchorMax = Vector2.one;
-        rectTransform.offsetMin = Vector2.zero;
-        rectTransform.offsetMax = Vector2.zero;
+        rectTransform.anchorMin = new Vector2(0, 0);
+        rectTransform.anchorMax = new Vector2(1, 1);
+        rectTransform.offsetMin = new Vector2(0, 0);
+        rectTransform.offsetMax = new Vector2(0, 0);
+
 
         finalScreenshotCanvas.SetActive(true);
         finalScreenShotCamera.SetActive(true);
+
+        UIManager.instance.chooseFramesPanel.SetActive(false);
 
         yield return new WaitForEndOfFrame();
 
@@ -242,30 +79,45 @@ public class MergePhotos : MonoBehaviour
         screenShot.Apply();
 
         scrCamera.targetTexture = null;
-        RenderTexture.active = null;
+        RenderTexture.active = null; // JC: added to avoid errors
         Destroy(rt);
         bytes = screenShot.EncodeToPNG();
 
-        Destroy(selectedFrame); // ✅ Important to avoid memory build-up
+        string directoryPath = (Application.dataPath + "/../Chosen Photos");
+        if (!Directory.Exists(directoryPath))
+        {
+            //if it doesn't, create it
+            Directory.CreateDirectory(directoryPath);
 
-        string directoryPath = Path.Combine(Application.dataPath, "../Chosen Photos");
-        if (!Directory.Exists(directoryPath)) Directory.CreateDirectory(directoryPath);
+        }
 
-        string allImagesDir = Path.Combine(Application.dataPath, "../All_Photos");
-        if (!Directory.Exists(allImagesDir)) Directory.CreateDirectory(allImagesDir);
+        Destroy(selectedFrame);
+
+        string allImagesDir = (Application.dataPath + "/../All_Photos");
+        if (!Directory.Exists(allImagesDir))
+        {
+            //if it doesn't, create it
+            Directory.CreateDirectory(allImagesDir);
+
+        }
 
         allImagesBytes.Clear();
 
-        // Step 2: Capture all frames
-        for (int i = 0; i < imageSlots.Length; i++)
+        foreach (GameObject img in imageSlots)
         {
-            GameObject currFrame = Instantiate(imageSlots[i], finalScreenshotCanvas.transform);
+            GameObject currFrame = Instantiate(img, finalScreenshotCanvas.transform);
 
             RectTransform _rectTransform = currFrame.GetComponent<RectTransform>();
-            _rectTransform.anchorMin = Vector2.zero;
-            _rectTransform.anchorMax = Vector2.one;
-            _rectTransform.offsetMin = Vector2.zero;
-            _rectTransform.offsetMax = Vector2.zero;
+            _rectTransform.anchorMin = new Vector2(0, 0);
+            _rectTransform.anchorMax = new Vector2(1, 1);
+            _rectTransform.offsetMin = new Vector2(0, 0);
+            _rectTransform.offsetMax = new Vector2(0, 0);
+
+
+            finalScreenshotCanvas.SetActive(true);
+            finalScreenShotCamera.SetActive(true);
+
+            UIManager.instance.chooseFramesPanel.SetActive(false);
 
             yield return new WaitForEndOfFrame();
 
@@ -278,80 +130,71 @@ public class MergePhotos : MonoBehaviour
             screenShot.Apply();
 
             scrCamera.targetTexture = null;
-            RenderTexture.active = null;
+            RenderTexture.active = null; // JC: added to avoid errors
             Destroy(_rt);
-
             byte[] b = screenShot.EncodeToPNG();
             allImagesBytes.Add(b);
-
-            Destroy(currFrame); // ✅ Prevents memory leak
-            yield return null;  // ✅ Allows Unity to breathe
         }
 
-        // Step 3: Save to disk and prepare form
-        string imageName = $"IMG_With_Chosen_Frame-{System.DateTime.Now:HH-mm-ss-dd-MM-yyyy}-choosen.png";
-        File.WriteAllBytes(Path.Combine(directoryPath, imageName), bytes);
-
         WWWForm form = new WWWForm();
+        //save locally
+        string imageName = "IMG_With_Chosen_Frame-" + System.DateTime.Now.ToString("HH-mm-ss-dd-MM-yyyy") + "choosen.png";
+        System.IO.File.WriteAllBytes(directoryPath + "/" + imageName, bytes);
         for (int i = 0; i < allImagesBytes.Count; i++)
         {
-            string _imageName = $"IMG_all_four-{System.DateTime.Now:HH-mm-ss-dd-MM-yyyy}_{i}.png";
-            File.WriteAllBytes(Path.Combine(allImagesDir, _imageName), allImagesBytes[i]);
-            form.AddBinaryData("images", allImagesBytes[i], _imageName, "image/png");
+            string _imageName = "IMG_all_four-" + System.DateTime.Now.ToString("HH-mm-ss-dd-MM-yyyy") + $"_{i}.png";
+            File.WriteAllBytes(allImagesDir + "/" + _imageName, allImagesBytes[i]);
+            form.AddBinaryData("images", allImagesBytes[i], imageName, "image/png");
         }
         form.AddField("title", "VISA");
         form.AddField("description", "Test description");
 
+        //form.AddBinaryData("image", bytes, imageName, "image/png");
         Debug.Log("Sending............");
 
-        UnityWebRequest request = UnityWebRequest.Post("https://sing-file-share.wskoly.xyz/api/upload/", form);
+        UnityWebRequest request = UnityWebRequest.Post($"https://sing-file-share.wskoly.xyz/api/upload/", form);
         request.SetRequestHeader("API-Key", API_KEY);
-
-        // Optional: add timeout logic
-        float startTime = Time.time;
-        float timeout = 10f;
-
         yield return request.SendWebRequest();
-
-        while (!request.isDone && Time.time - startTime < timeout)
-            yield return null;
 
         finalScreenshotCanvas.SetActive(false);
         finalScreenShotCamera.SetActive(false);
 
-        if (request.result == UnityWebRequest.Result.Success)
+        if (!request.isNetworkError && !request.isHttpError)
         {
             string responseText = request.downloadHandler.text;
             Debug.Log(responseText);
+            SingFileApiResponse response = null;
             try
             {
-                var response = JsonConvert.DeserializeObject<SingFileApiResponse>(responseText);
-                if (response != null)
-                {
-                    string url = response.link;
-                    QR_DisplayQuad.GetComponent<EasyQRCode>().textToEncode = url;
-                    QR_DisplayQuad.SetActive(true);
-                    finalPanel.SetActive(true);
-                }
+                response = JsonConvert.DeserializeObject<SingFileApiResponse>(responseText);
             }
             catch (System.Exception e)
             {
-                Debug.LogError("JSON Parse Error: " + e.Message);
+                Debug.Log(e.Message);
+            }
+
+            if (response != null)
+            {
+                string url = response.link;
+                //imageUrlText.text = url;
+                QR_DisplayQuad.GetComponent<EasyQRCode>().textToEncode = url;
+                //call to generate QRCode here
+                QR_DisplayQuad.SetActive(true);
+                finalPanel.SetActive(true);
             }
         }
         else
         {
-            Debug.LogError("Upload Error: " + request.error);
+            Debug.Log(request.error);
+            Debug.Log(request.downloadHandler.text);
             NetworkError();
         }
 
         UIManager.instance.finalPanel.SetActive(true);
     }
 
-
     void NetworkError()
     {
         //networkErrorText.SetActive(true);
-        Debug.LogError("Network error hoiseeeeeeeeeeee");
     }
 }
